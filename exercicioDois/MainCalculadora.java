8d091397720f1c24a8250135b2bfeb0e7fbd5de1
Gilnei Lima <gilneilima@hotmail.com>
2021-09-28 18:01:52 -0300

    Add files via upload

---
+++ exercicioDois/MainCalculadora.java
@@ -0,0 +1,9 @@
+package exercicioDois;
+
+public class MainCalculadora {
+
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        calc.Calcular();
+    }
+}
